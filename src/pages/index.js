ff8e588c6c5c1b219cc94bc2b7968030a621934f
JeremyDevCode <jeremydevcode@gmail.com>
2023-03-28 22:19:14 -0500

    Adding social card metadata

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,15 +16,31 @@ export default function Home() {
         className={`sm:w-full w-full sm:h-fit flex items-center justify-center dark:bg-bgBlack bg-bgWhite`}
       >
         <Head>
-          <meta property="og:title" content="Jeremy <Frontend/>" />
-          <meta property="title" content="Jeremy <Frontend/>" />
+          <title>{"Jeremy <Frontend/>"}</title>
           <meta
             name="description"
-            content="Jeremy Mosquera Frontend Developer Portfolio testing text only for test the metadata"
+            content="Jeremy Mosquera Frontend Developer Portfolio"
           ></meta>
+
+          <meta itemProp="name" content="Jeremy <Frontend/>" />
+          <meta
+            itemProp="description"
+            content="Jeremy Mosquera Frontend Developer Portfolio testing text only for test the metadata"
+          />
+          <meta
+            itemProp="image"
+            content="http://res.cloudinary.com/deohsoirn/image/upload/v1680056455/Portfolio/additionalImages/myHouse_wyasnd.png"
+          />
+
+          <meta
+            property="og:url"
+            content="https://jeremyportfolio.vercel.app"
+          />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Jeremy <Frontend/>" />
           <meta
             name="og:description"
-            content="Jeremy Mosquera Frontend Developer Portfolio testing text only for test the metadata"
+            content="Jeremy Mosquera Frontend Developer Portfolio"
           ></meta>
           <meta
             property="og:image"
@@ -33,6 +49,17 @@ export default function Home() {
           <meta property="og:image:type" content="image/png" />
           <meta property="og:image:width" content="1200" />
           <meta property="og:image:height" content="630" />
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="Jeremy <Frontend/>" />
+          <meta
+            name="twitter:description"
+            content="Jeremy Mosquera Frontend Developer Portfolio"
+          />
+          <meta
+            name="twitter:image"
+            content="http://res.cloudinary.com/deohsoirn/image/upload/v1680056455/Portfolio/additionalImages/myHouse_wyasnd.png"
+          />
         </Head>
 
         <div className="sm:px-[15%]">
