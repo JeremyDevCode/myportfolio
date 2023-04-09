745ecb8f191674c799da3f6ca8049bbfad48cf6c
JeremyDevCode <jeremydevcode@gmail.com>
2023-04-08 19:17:49 -0500

    Fix techCard width and height

--- src/assets/cards/TechCard.jsx
+++ src/assets/cards/TechCard.jsx
@@ -6,9 +6,9 @@ function TechCard({ technology }) {
     <li className="flex items-center gap-2 text-sm dark:text-secondaryBText text-secondaryWText">
       <Image
         src={technology.icon}
-        className="w-auto h-4 object-cover"
-        width={4}
-        height={4}
+        className="w-auto h-4"
+        width={100}
+        height={100}
         alt={technology.name}
         loading="lazy"
       />
