7fd0c24958c11a0162069e3b2993e5aa5af43535
Jeremy <jeremydevcode@gmail.com>
2024-05-08 22:24:53 -0500

    fix(next): hide button that is not yet finished with a feature flag

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,6 +3,9 @@ import { ProjectCard } from "../assets/cards/ProjectCard";
 import projects from "../data/projects.json";
 import { StarIcon } from "../assets/icons/StarIcon";
 import { NormalStarIcon } from "../assets/icons/NormalStarIcon";
+import { CONSTANTS } from "../../constants";
+
+const { WITH_SNEAK_PEEKS } = CONSTANTS;
 
 function Projects() {
   useEffect(() => {
@@ -42,11 +45,13 @@ function Projects() {
         <h1 className="font-extrabold lg:text-5xl text-4xl dark:text-primaryBText primaryWText">
           Projects
         </h1>
-        <button className="mt-[-40px] flex items-center justify-center gap-5 dark:bg-[#0D0F12] md:border-2 border-[1px] dark:border-[#999] border-[#0D0F12] bg-[#F2F2F2] text-[#0D0F12] dark:text-[#E3E6E8] py-2 px-4 rounded-lg dark:hover:border-[#FFF159] hover:border-[#51D1F6] dark:hover:text-[#FFF159] hover:text-[#51D1F6] transition-all text-[0.9rem] md:text-base text-xs font-semibold">
-          <NormalStarIcon />
-          <StarIcon />
-          <span>Sneak Peeks</span>
-        </button>
+        {WITH_SNEAK_PEEKS && (
+          <button className="mt-[-40px] flex items-center justify-center gap-5 dark:bg-[#0D0F12] md:border-2 border-[1px] dark:border-[#999] border-[#0D0F12] bg-[#F2F2F2] text-[#0D0F12] dark:text-[#E3E6E8] py-2 px-4 rounded-lg dark:hover:border-[#FFF159] hover:border-[#51D1F6] dark:hover:text-[#FFF159] hover:text-[#51D1F6] transition-all text-[0.9rem] md:text-base text-xs font-semibold">
+            <NormalStarIcon />
+            <StarIcon />
+            <span>Sneak Peeks</span>
+          </button>
+        )}
       </div>
       <div className="grid gap-8 lg:grid-cols-2 place-items-center">
         {projects.map((project) => (
